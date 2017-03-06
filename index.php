8cf764c050ee39cca6329ca1c950e2b18c03a3bd
Leon-ardo-da-vin-ci <daraz.leon@gmail.com>
2017-03-06 15:31:30 +0100

    Index
    (design)

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Imperial Creativity</title>
+<meta charset="UTF-8">
+<link href="couscous/css/bootstrap.min.css" rel="stylesheet" media="screen" />
+<link href="couscous/css/bootstrap-theme.min.css" rel="stylesheet" media="screen" />
+<link rel="stylesheet" href="couscous/couscous.css" type="text/css" />
+<script type="text/javascript" src="jquery-1.11.3-jquery.min.js"></script>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700">
+</head>
+
+<body id="mainpage">
+
+<!-- Bootstrap Navigation (Couscous-Design)-->
+<nav id="navimp" class="navbar navbar-default">
+<div id="imp-site">
+  <div class="container-fluid">
+
+    <div class="navbar-header">
+    <!-- mobile button -->
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <!-- Logo -->
+      <a id="imp-logo" class="navbar-brand" href="index.php">
+      <span class="glyphicon glyphicon-globe" aria-hidden="true" style="float: left;margin-right: 10px;"></span>Imperial <div class="second-color">Creativity</div></font></a>
+    </div>
+
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="imp-active"><a href="index.php">Home</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Products <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+
+      <!-- search -->
+      <form class="navbar-form navbar-left">
+        <div class="form-group">
+          <input type="text" class="imp-search" placeholder="Search">
+        </div>
+        <button type="submit" class="btn impbtn-default"><span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+</button>
+      </form>
+      <!-- search -->
+
+      <!-- right side / login / user -->
+      <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Log In <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#"></a></li>
+          </ul>
+        </li>
+      </ul>
+
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+  </div>
+</nav>
+
+test
+
+
+<!-- Load JS on the bottom of the page (for faster loading speed) -->
+<script src="couscous/js/bootstrap.min.js"></script>
+
+</body>
